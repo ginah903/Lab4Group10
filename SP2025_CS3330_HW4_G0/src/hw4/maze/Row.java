32e77fbc814f5479a8c8f7a5a819e719eb758dc4
ginah903 <ginah903@gmail.com>
2025-04-21 19:15:12 -0500

    Adding Row.java file with javadoc

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Row.java
@@ -5,10 +5,16 @@ import java.util.List;
 public class Row {
 	private List<Cell> cells;
 	
+	/**
+	 * Creates a Row with a given list of cells.
+	 * @param cells
+	 */
 	public Row(List<Cell> cells) {
 		this.cells = cells;
 	}
 	
+	
+	// GETTERS AND SETTERS
 	public List<Cell> getCells() {
 		return cells;
 	}
@@ -19,9 +25,13 @@ public class Row {
 		
 	}
 
+	/**
+	 * Returns a string representation of the Row, lists out all the cells 
+	 * in the same format as the test file. 
+	 */
 	@Override
 	public String toString() {
-		return "Row [cells = " + cells + "]";
+		return "Row [cells=" + cells + "]";
 	}
 	
 	
