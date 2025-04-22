c27baf3bf01e4f39faf3aa0cbfe769b8ef5898c5
jackeveker <jackeveker@wi000010fi.dhcp.missouri.edu>
2025-04-22 10:55:44 -0500

    added Grid.java file with javadoc

--- SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
+++ SP2025_CS3330_HW4_G0/src/hw4/maze/Grid.java
@@ -1,5 +1,30 @@
 package hw4.maze;
 
 public class Grid {
-
+	private List<Row> rows;
+	
+	/**
+	 * Creates a Grid with a given list of rows.
+	 * @param rows
+	 */
+	public Grid(List<Row> rows) {
+		this.rows = rows;
+	}
+	
+	//GETTERS AND SETTERS
+	public List<Row> getRows(){
+		return rows;
+	}
+	
+	public void setRows(List<Row> rows) {
+		this.rows = rows;
+	}
+	
+	/**
+	 * Returns a string representation of the Grid, lists out all the rows.
+	 */
+	@Override
+	public String toString() {
+		return "Grid [rows=" + rows + "]";
+	}
 }
