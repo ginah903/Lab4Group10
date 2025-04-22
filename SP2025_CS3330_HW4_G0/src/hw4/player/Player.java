8644744fc97b77d60b4fc52df95302eb31314846
jackeveker <jackeveker@wi000010fi.dhcp.missouri.edu>
2025-04-22 11:36:19 -0500

    added Player.java content with javadoc

--- SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
+++ SP2025_CS3330_HW4_G0/src/hw4/player/Player.java
@@ -1,5 +1,38 @@
 package hw4.player;
 
 public class Player {
+	private Row currentRow;
+	private Cell currentCell;
+	
+	
+	//constructor for player
+	public Player(Row currentRow, Cell currentCell) {
+		this.currentRow = currentRow;
+		this.currentCell = currentCell;
+	}
+	
+	//GETTERS AND SETTERS
+	public Row getCurrentRow() {
+        return currentRow;
+    }
 
+    public void setCurrentRow(Row currentRow) {
+        this.currentRow = currentRow;
+    }
+
+    public Cell getCurrentCell() {
+        return currentCell;
+    }
+
+    public void setCurrentCell(Cell currentCell) {
+        this.currentCell = currentCell;
+    }
+    
+    /**
+	 * Returns a string representation of the Player, lists out all the rows and cells.
+	 */
+    @Override
+    public String toString() {
+    	return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
+    }
 }
